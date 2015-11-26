ea6816ede404fd278d89aeae4f2284dd4f469e9d
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-11-26 21:34:08 -0200

    first version, seems to be not working

--- main.c
+++ main.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define updateDelta() delta = fmaxf(hmax, pmax)/255
-#define value(h, d) (float) ceil(h/d)
+#define value(h, d) (int) ceil(h/d)
 #define t(step, timestep) step*timestep
 
 typedef unsigned int ui;
-
-typedef struct{
-  int blue;
-  int red;
-} Color;
 
 typedef struct{
   int x, y;
   int step;
 } drop;
 
+typedef struct{
+  float average, sqrSum;
+} Point;
+
 /* Global Variables */
 ui larg, alt, L, H, T, v, N, s, steps, ndrops=0, pdistx, pdisty;
 int procs, **lake;
-Color **colors;
 drop *drops;
+Point **points;
 float e, P, timestep, hmax, pmax, delta;
-FILE * PPMFile, averageFile;
+FILE *PPMFile, *averageFile;
 
 
 /*h(p,t)=(p-vt)e^(-(p-vt)^2-t/10)*/
@@ -34,24 +32,30 @@ float h(float p, float t)
   return d*exp(-d*d-t/10);
 }
 
-FILE * createPPM()
+void createOutputFiles()
 {
   PPMFile = fopen ("out.ppm","w");
+  averageFile = fopen ("out.txt","w");
   if (PPMFile!=NULL) {
     fprintf(PPMFile, "P3\n%d %d\n255\n", larg, alt);
   }
-  return PPMFile;
+}
+
+void closeOutputFiles() {
+  fclose(PPMFile);
+  fclose(averageFile);
 }
 
 void updatePoint(ui i, ui j)
 {
   float height = 0;
   ui k;
+  int x, y;
   /* Paralelizável */
   for (k = 0; k < ndrops; ++k) {
-    /* TODO for each drop calculate distance sqrt(dx^2+dy^2) from (i,j) */
-    /* TODO calculate before and store the distances between points and drops */
-    /* TODO For each distance calculate h() and sum to height */
+    x = (drops[k].x - i) * pdistx;
+    y = (drops[k].y - j) * pdisty;
+    height += h(sqrt(x*x+y*y), t(steps, timestep));
   }
   if (height < e && height > -e) {
     lake[i][j] = 0;
@@ -60,19 +64,24 @@ void updatePoint(ui i, ui j)
   if (height > hmax) hmax = height;
   else if (-height > pmax) pmax = height;
   lake[i][j] = height;
+  points[i][j].average += height;
+  points[i][j].sqrSum += height*height;
 }
 
-void PPM(int i, int j)
+void evaluatePointForPPM(ui i, ui j)
 {
   if (lake[i][j] > 0) {
-    colors[i][j].red = 0;
-    colors[i][j].blue = value(lake[i][j], delta);
+    if (j == 0)
+      fprintf(PPMFile, "0 0 %d", value(lake[i][j], delta));
+    else
+      fprintf(PPMFile, " 0 0 %d", value(lake[i][j], delta));
   } else {
-    colors[i][j].blue = 0;
-    colors[i][j].red = value(lake[i][j], delta);
+    if (j == 0)
+      fprintf(PPMFile, "%d 0 0", value(lake[i][j], delta));
+    else
+      fprintf(PPMFile, " %d 0 0", value(lake[i][j], delta));
   }
 
-  /* TODO print color to PPMFile R G B */
 }
 
 int main(int argc, char const *argv[])
@@ -94,20 +103,25 @@ int main(int argc, char const *argv[])
 
     /* Mallocs */
     lake = (int**) malloc (L*sizeof(int*));
-    colors = (Color**) malloc (L*sizeof(Color*));
+    points = (Point**) malloc (L*sizeof(Point*));
     for (i = 0; i < L; ++i) {
       lake[i] = (int*) malloc (H*sizeof(int));
-      colors[i] = (Color*) malloc (H*sizeof(Color));
+      points[i] = (Point*) malloc (H*sizeof(Point));
     }
     drops = (drop*) malloc (N*sizeof(drop));
 
+
+    pdistx = (float) larg / (float) L;
+    pdisty = (float) alt / (float) H;
+
     for (steps = 0; steps < N; ++steps) { /* Paralelizável, mas prioridade baixa */
+
       pmax = 0;
       hmax = 0;
 
       /* Atualiza os pontos */
-      for(i = 0; i < larg; ++i) { /* Paralelizável */
-        for (j = 0; j < alt; ++j) { /* Paralelizável */
+      for(i = 0; i < L; ++i) { /* Paralelizável */
+        for (j = 0; j < H; ++j) { /* Paralelizável */
           updatePoint(i, j);
         }
       }
@@ -123,16 +137,23 @@ int main(int argc, char const *argv[])
 
     }
 
-    updateDelta();
+    delta = fmaxf(hmax, pmax)/255;
 
-    createPPM();
-    /* TODO Calculate Average and STDDEV */
-    for(i = 0; i < larg; ++i) { /* Paralelizável */
-      for (j = 0; j < alt; ++j) { /* Paralelizável */
-        PPM(i, j);
+
+    createOutputFiles();
+    for(i = 0; i < L; ++i) { /* Paralelizável */
+      for (j = 0; j < H; ++j) { /* Paralelizável */
+
+        evaluatePointForPPM(i, j);
+        /* Average and StdDev */
+        points[i][j].average /= N;
+        fprintf(averageFile, "%d %d %12.7f %f\n", i, j, points[i][j].average, sqrt(points[i][j].sqrSum/N - points[i][j].average*points[i][j].average));
       }
-      /* TODO print \n to PPMFile */
+      fprintf(PPMFile, "\n");
     }
+
+
+    closeOutputFiles();
   }
   return 0;
 }
